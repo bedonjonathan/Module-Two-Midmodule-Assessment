04ccd00d99f3baa39198a503107523a9aaf1d1c1
Jonathan Bedon <bedonjonathan@gmail.com>
2021-02-28 13:42:15 -0500

    first pass of test

---
+++ dice.js
@@ -0,0 +1,29 @@
+
+const diceFaces = ["&#9856", "&#9857", "&#9858", "&#9859 ", "&#9860", "&#9861"];
+const form = document.querySelector("form");
+const numOfDice = document.querySelector("#num-of-dice");
+const rollButton = document.querySelector("#rollButton");
+let diceResult = document.querySelector("#dice-para");
+let sumText = document.querySelector("#sum");
+const originalSumtext = sumText.textContent;
+let ul = document.querySelector("#history-list");
+let dice;
+
+form.button.addEventListener("click", (e) => {
+  e.preventDefault();
+  let sum = 0
+  sumText.textContent = originalSumtext
+  dice = "";
+  for (let i = 0; i < Number(numOfDice.value); i++) {
+    let randomNum = Math.floor(Math.random() * diceFaces.length)
+    dice += diceFaces[randomNum];
+    sum += (randomNum+1)
+    }
+  diceResult.innerHTML = dice;
+  sumText.textContent += sum
+  let li = document.createElement("li");
+  li.innerHTML += dice;
+  li.innerText += ` = ${sum}`
+  ul.appendChild(li);
+
+});
