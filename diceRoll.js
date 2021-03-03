f6f318d73a3b6170c3a766fb95652e2ef7a0a062
vrwatson <vanessawatson@pursuit.org>
2021-03-02 19:37:11 -0500

    refactoring assessment

--- diceRoll.js
+++ diceRoll.js
@@ -1,18 +1,12 @@
 const dice = [
-  { entity: "9856", icon: "⚀", value: 1 },
-  { entity: "9857", icon: "⚁", value: 2 },
-  { entity: "9858", icon: "⚂", value: 3 },
-  { entity: "9859", icon: "⚃", value: 4 },
-  { entity: "9860", icon: "⚄", value: 5 },
-  { entity: "9861", icon: "⚅", value: 6 },
+  { 1: "⚀" },
+  { 2: "⚁" },
+  { 3: "⚂" },
+  { 4: "⚃" },
+  { 5: "⚄" },
+  { 6: "⚅" },
 ];
 
-// &#9856 = ⚀  1
-// &#9857 = ⚁  2
-// &#9858 = ⚂  3
-// &#9859 = ⚃  4
-// &#9860 = ⚄  5
-// &#9861 = ⚅  6
 
 let rollInput = document.querySelector("#number-of-dice");
 const rollResult = document.querySelector("#dice-para");
